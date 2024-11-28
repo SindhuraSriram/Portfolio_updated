fa527ddd06f97a3f5410773563f83d885a5e4191
SindhuraSriram <42845151+SindhuraSriram@users.noreply.github.com>
2024-11-28 09:02:23 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider, FilledContext } from 'react-helmet-async';
 import { ThemeProvider } from './contexts/ThemeContext';
 import App from './App.tsx';
 import './index.css';
 
+const helmetContext = {};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
+    <HelmetProvider context={helmetContext}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
